dda145b7defd091302ec4f23cf09c60e13cde6b2
himanshu chablani <himanshu.neo92@gmail.com>
2015-01-25 23:01:33 +0000

    s build has read and write func for bmp files

--- octet/src/examples/TerrainGeneration/terrain_gen.h
+++ octet/src/examples/TerrainGeneration/terrain_gen.h
@@ -8,6 +8,9 @@
 #ifndef TERRAIN_GEN_H_INCLUDED
 #define TERRAIN_GEN_H_INCLUDED
 
+#include<iostream>
+#include<fstream>
+
 namespace octet {
 
   /// Scene containing a box with octet.
@@ -31,7 +34,7 @@ namespace octet {
     };
     static uint32_t make_color(float r, float g, float b) {
       return 0xff000000 + ((int)(r*255.0f) << 0) + ((int)(g*255.0f) << 8) + ((int)(b*255.0f) << 16);
-    }    
+    }
     void setup_camera()
     {
       mat4t &camera_mat = app_scene->get_camera_instance(0)->get_node()->access_nodeToParent();
@@ -94,69 +97,69 @@ namespace octet {
       /*
       if (is_key_down(key::key_up))
       {
-        switch (selected_attrib)
-        {
-        case 0: if (ice_pert_from <= ice_pert_to && ice_pert_from >= 0.0f)
-          ice_pert_from += 0.01f;
-          break;
-        case 1:
-          ice_pert_to += 0.01f;
-          break;
+      switch (selected_attrib)
+      {
+      case 0: if (ice_pert_from <= ice_pert_to && ice_pert_from >= 0.0f)
+      ice_pert_from += 0.01f;
+      break;
+      case 1:
+      ice_pert_to += 0.01f;
+      break;
 
-        case 2:
-          if (water_pert_from <= water_pert_to)
-            water_pert_from += 0.01f;
-          break;
-        case 3:
-          if (water_pert_to <= grass_pert_from)
-            water_pert_to += 0.01f;
-          break;
+      case 2:
+      if (water_pert_from <= water_pert_to)
+      water_pert_from += 0.01f;
+      break;
+      case 3:
+      if (water_pert_to <= grass_pert_from)
+      water_pert_to += 0.01f;
+      break;
 
-        case 4:
-          if (grass_pert_from <= grass_pert_to)
-          {
-            grass_pert_from += 0.01f;
-          }
-          break;
-
-        case 5:
-          if (grass_pert_to <= ice_pert_from)
-          {
-            grass_pert_to += 0.01f;
-          }
-          break;
-        }
+      case 4:
+      if (grass_pert_from <= grass_pert_to)
+      {
+      grass_pert_from += 0.01f;
       }
-      
+      break;
+
+      case 5:
+      if (grass_pert_to <= ice_pert_from)
+      {
+      grass_pert_to += 0.01f;
+      }
+      break;
+      }
+      }
+
       if (is_key_down(key::key_down))
       {
-        switch (selected_attrib)
-        {
-        case 0: if (ice_pert_from >= grass_pert_to)
-          ice_pert_from -= 0.01f;
-          break;
-        case 1:if (ice_pert_to >= ice_pert_from)
-          ice_pert_to -= 0.01f;
-          break;
-        case 2:if (water_pert_from >= 0.01f)
-          water_pert_from -= 0.01f;
-          break;
-        case 3: if (Game_UI.water_pert_to >= water_pert_from)
-        {
-          Game_UI.water_pert_to -= 0.01f;
-        }
-                break;
-        case 4: if (Game_UI.grass_pert_from >= Game_UI.water_pert_to)
-        {
-          Game_UI.grass_pert_from -= 0.01f;
-        }
-                break;
-        case 5: if (grass_pert_to >= Game_UI.grass_pert_from)
-        {
-          grass_pert_to -= 0.01f;
-        }
-                break;
-        }
+      switch (selected_attrib)
+      {
+      case 0: if (ice_pert_from >= grass_pert_to)
+      ice_pert_from -= 0.01f;
+      break;
+      case 1:if (ice_pert_to >= ice_pert_from)
+      ice_pert_to -= 0.01f;
+      break;
+      case 2:if (water_pert_from >= 0.01f)
+      water_pert_from -= 0.01f;
+      break;
+      case 3: if (Game_UI.water_pert_to >= water_pert_from)
+      {
+      Game_UI.water_pert_to -= 0.01f;
+      }
+      break;
+      case 4: if (Game_UI.grass_pert_from >= Game_UI.water_pert_to)
+      {
+      Game_UI.grass_pert_from -= 0.01f;
+      }
+      break;
+      case 5: if (grass_pert_to >= Game_UI.grass_pert_from)
+      {
+      grass_pert_to -= 0.01f;
+      }
+      break;
+      }
       }
       */
       if (is_key_going_up(key::key_right))
@@ -173,12 +176,12 @@ namespace octet {
         else
           Game_UI.selected_attrib--;
       }
-      
+
     }
   public:
     /// this is called when we construct the class before everything is initialised.
     terrain_gen(int argc, char **argv) : app(argc, argv) {
-    }    
+    }
     /// this is called once OpenGL is initialized
     void app_init() {
       app_scene = new visual_scene();
@@ -190,7 +193,7 @@ namespace octet {
       int vx = 0, vy = 0;
       get_viewport_size(vx, vy);
       Game_UI.initUI(vx, vy);
-           
+
       inputs.init(this);
       img_gen.init();
       generate(false, false);
@@ -215,7 +218,38 @@ namespace octet {
       Game_UI.pop_up_clear();
       input_keys();
     }
+    void make_bmp_file(ofstream *s)
+    {
+      s->put(char(66));//b
+      s->put(char(77));//m
+      for (int i = 0; i < 8; i++){
+        s->put(char(0));//8 spaces
 
+      }
+      s->put(char(54));//6
+
+      for (int i = 0; i < 3; i++){
+        s->put(char(0));//3 spaces
+      }
+      s->put(char(40));//(
+      for (int i = 0; i < 3; i++){
+        s->put(char(0));//3 spaces
+      }
+      s->put(char(height_image % 256));
+      s->put(char((height_image >> 8) % 256));
+      s->put(char((height_image >> 16) % 256));
+      s->put(char((height_image >> 24) % 256));
+      s->put(char(width_image % 256));
+      s->put(char((width_image >> 8) % 256));
+      s->put(char((width_image >> 16) % 256));
+      s->put(char((width_image >> 24) % 256));
+      s->put(char(1));//start of header
+      s->put(char(0));//space
+      s->put(char(24));//cancle 
+      for (int i = 0; i < 25; i++){
+        s->put(char(0));//25 spaces
+      }
+    }
     void generate(bool from_image, bool random_seed){
 
       //the mesh generatiion
@@ -223,8 +257,8 @@ namespace octet {
       material *red = new material(vec4(0.6f, 0.298f, 0.0f, 1.0f), shader);
       mesh *terrain = new mesh();
       // allocate vertices and indices into OpenGL buffers
-      size_t num_vertices = height_image * width_image; 
-      size_t num_indices = 6*(num_vertices - height_image) - width_image;
+      size_t num_vertices = height_image * width_image;
+      size_t num_indices = 6 * (num_vertices - height_image) - width_image;
       terrain->allocate(sizeof(my_vertex)* num_vertices, sizeof(uint32_t)* num_indices);
       terrain->set_params(sizeof(my_vertex), num_indices, num_vertices, GL_TRIANGLES, GL_UNSIGNED_INT);
       // describe the structure of my_vertex to OpenGL
@@ -236,24 +270,80 @@ namespace octet {
       gl_resource::wolock il(terrain->get_indices());
       uint32_t *idx = idx = il.u32();
       //end of init
+
+
       float min = 0.0f, max = 0.0f;
       float *image = new float[height_image*width_image];
-      pn.fill_image(image,min, max, 16);
+      ofstream grey;
+      ofstream s;
+      ifstream grey_read("Height_Map.bmp", std::ios::in | std::ios::binary);
+
+      if (!from_image)
+      {
+      pn.fill_image(image, min, max, 16);      
+      s.open("colour_map.bmp", ofstream::binary);
+      if (!(s.is_open())){
+        printf("Cannot open the file...");
+        exit(0);
+      }
+      make_bmp_file(&s);
+      
+      grey.open("perlin_noise_grey.bmp", ofstream::binary);
+      if (!(grey.is_open())){
+        printf("Cannot open the file...");
+        exit(0);
+      }
+      make_bmp_file(&grey);
+      }
+      
+      if (grey_read.is_open()&&from_image)
+      {
+        char *line = new char[54];
+        grey_read.read(line,54);
+        printf("The line was %s",line);
+        if (line!="BM")
+        {
+        printf("The file is corrupt");        
+       // exit(0);
+        }
+      }
       // Visit every pixel of the image and assign a color generated with Perlin noise
       for (int i = 0; i < height_image; ++i) {     // z
-        for (int j = 0; j < width_image; ++j) {  // x   
-          float vert_height = *(image + i*width_image + j)*100.0f;                     
-          vtx->pos = vec3p((float)j, vert_height, (float)i);
-          vtx->nor = vec3p((float)j, vert_height, (float)i);
-          vec3 mesh_colour = img_gen.create_colour(*(image + i*width_image + j), min, max);
-          vtx->color = make_color(mesh_colour.x(),mesh_colour.y(),mesh_colour.z());        
-          vtx++;                    
+        for (int j = 0; j < width_image; ++j) {  // x
+          if (!from_image)
+          {
+            float vert_height = *(image + i*width_image + j)*100.0f;
+            vtx->pos = vec3p((float)j, vert_height, (float)i);
+            vtx->nor = vec3p((float)j, vert_height, (float)i);
+            vec3 mesh_colour = img_gen.create_colour(*(image + i*width_image + j), min, max);
+            vtx->color = make_color(mesh_colour.x(), mesh_colour.y(), mesh_colour.z());
+            s.put(char(mesh_colour.z() * 255));
+            s.put(char(mesh_colour.y() * 255));
+            s.put(char(mesh_colour.x() * 255));
+            grey.put(char(vert_height * 255 / 100));
+            grey.put(char(vert_height * 255 / 100));
+            grey.put(char(vert_height * 255 / 100));
           }
+          else
+          {
+            char aux;
+            grey_read.read(&aux,1);
+            grey_read.read(&aux, 1);
+            grey_read.read(&aux, 1);
+
+            float vert_height = (float)((aux*100.0f)/255.0f);
+            vtx->pos = vec3p((float)j, vert_height, (float)i);
+            vtx->nor = vec3p((float)j, vert_height, (float)i);
+            vec3 mesh_colour = img_gen.create_colour(*(image + i*width_image + j), min, max);
+            vtx->color = make_color(mesh_colour.x(), mesh_colour.y(), mesh_colour.z());
           }
-          //make triangles          
+          vtx++;
+        }
+      }
+      //make triangles          
       int tempvert = 0;
-      for (int i=0;i<height_image-1; ++i) {
-        for (int j=0;j<width_image-1; ++j) {
+      for (int i = 0; i < height_image - 1; ++i) {
+        for (int j = 0; j < width_image - 1; ++j) {
 
           idx[0] = tempvert;
           idx[1] = tempvert + 1;
@@ -273,8 +363,12 @@ namespace octet {
       scene_node *node = new scene_node();
       app_scene->add_child(node);
       app_scene->add_mesh_instance(new mesh_instance(node, terrain, red));
-      delete []image;
-    }   
+      s.close();
+      grey.close();
+      delete[]image;
+    }
+
+
   };
 }
 #endif
